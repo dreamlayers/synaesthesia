b90b17be17c57aab7da11abea2cf75573eefe990
Boris Gjenero <boris.gjenero@gmail.com>
2013-12-09 21:13:37 -0500

    Change ordinary fading so it fades all the way down to 0.
    Ordinary fading did not fade all the way down to 0. This was not
    a problem with 8 bpp output because it uses 4 bits of each of the
    two calculated components. That ignored the bits that stayed set.
    32 bpp output uses all 8 bits and so fading to 0 is needed.

--- core.cc
+++ core.cc
@@ -169,15 +169,43 @@ inline void addPixelFast(unsigned char *p,int br1,int br2) {
 }
 
 void fadeFade() {
-  register uint32_t *ptr = (uint32_t*)output;
-  int i = outWidth*outHeight*2/sizeof(uint32_t);
+  /* The fade calculations were unsuitable for 32 bpp output
+   * because they did not fade all the way down to 0.
+   * This table matches the bytewise version below, except at the
+   * low end, where each brightness is decreased by at least one.
+   */
+  static const uint8_t fadetab[256] = {
+    0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    15,16,17,18,19,20,20,21,22,23,24,25,26,27,28,29,
+    29,30,31,32,33,34,35,36,37,38,39,39,40,41,42,43,
+    44,45,46,47,48,49,49,50,51,52,53,54,55,56,57,58,
+    58,59,60,61,62,63,64,65,66,67,68,68,69,70,71,72,
+    73,74,75,76,77,78,78,79,80,81,82,83,84,85,86,87,
+    87,88,89,90,91,92,93,94,95,96,97,97,98,99,100,101,
+    102,103,104,105,106,107,107,108,109,110,111,112,113,114,115,116,
+    116,117,118,119,120,121,122,123,124,125,126,126,127,128,129,130,
+    131,132,133,134,135,136,136,137,138,139,140,141,142,143,144,145,
+    145,146,147,148,149,150,151,152,153,154,155,155,156,157,158,159,
+    160,161,162,163,164,165,165,166,167,168,169,170,171,172,173,174,
+    174,175,176,177,178,179,180,181,182,183,184,184,185,186,187,188,
+    189,190,191,192,193,194,194,195,196,197,198,199,200,201,202,203,
+    203,204,205,206,207,208,209,210,211,212,213,213,214,215,216,217,
+    218,219,220,221,222,223,223,224,225,226,227,228,229,230,231,232
+  };
+  register uint8_t *ptr = (uint8_t*)output;
+  int i = outWidth*outHeight*2;
   do {
+    uint8_t x = *ptr;
+    *(ptr++) = fadetab[x];
     //Bytewize version was: *(ptr++) -= *ptr+(*ptr>>1)>>4;
+#if 0
+    //Optimized 32 bit version processing 4 bytes at a time was:
     uint32_t x = *ptr;
     if (x)
         *(ptr++) = x - ((x & 0xf0f0f0f0ul) >> 4) - ((x & 0xe0e0e0e0ul) >> 5);
     else
       ptr++;
+#endif
   } while(--i > 0);
 }
  
